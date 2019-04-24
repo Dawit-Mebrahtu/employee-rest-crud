79b0dc76b9855c1f573528a2f00de74b8234d0bc
Dawit Mebrahtu <dawitaraya7@gmail.com>
2019-04-24 11:39:36 -0500

    in-memory authentication

---
+++ src/main/java/edu/mum/employeerestcrud/config/SecurityConfig.java
@@ -0,0 +1,65 @@
+package edu.mum.employeerestcrud.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.User.UserBuilder;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+
+        UserBuilder users = User.withDefaultPasswordEncoder();
+
+        auth.inMemoryAuthentication()
+                .withUser(users.username("dave").password("test123").roles("EMPLOYEE"))
+                .withUser(users.username("betty").password("test123").roles("EMPLOYEE", "MANAGER"))
+                .withUser(users.username("admin").password("test123").roles("EMPLOYEE", "ADMIN"));
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+
+//        http.authorizeRequests()
+//                .antMatchers("/api/employees*").hasAnyRole("MANAGER", "ADMIN")
+//                .antMatchers("/customer/save*").hasAnyRole("MANAGER", "ADMIN")
+//                .antMatchers("/customer/delete").hasRole("ADMIN")
+//                .antMatchers("/customer/**").hasRole("EMPLOYEE")
+//                .antMatchers("/resources/**").permitAll()
+//                .and()
+//                .formLogin()
+//                .loginPage("/login")
+//                .loginProcessingUrl("/authenticateTheUser")
+//                .permitAll()
+//                .and()
+//                .logout().permitAll()
+//                .and()
+//                .exceptionHandling().accessDeniedPage("/access-denied");
+
+
+        http.authorizeRequests()
+                .anyRequest().authenticated()
+                .and()
+                .formLogin()
+                .loginPage("/login")
+                .loginProcessingUrl("/authenticateTheUser")
+                .permitAll()
+                .and()
+                .logout().permitAll();
+
+    }
+
+}
+
+
+
+
+
+
+
---
+++ src/main/resources/data.sql
@@ -0,0 +1,3 @@
+INSERT INTO employee(id, first_name, last_name, email) VALUES (1, 'Betty', 'Bete', 'bbete@mum.edu');
+INSERT INTO employee(id, first_name, last_name, email) VALUES (2, 'Dave', 'Mebrahtu', 'dmebrahtu@mum.edu');
+ 
