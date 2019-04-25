6acfa99e0e1d525618494767c8f7303214c9aa2f
Dawit Mebrahtu <dawitaraya7@gmail.com>
2019-04-25 12:15:41 -0500

    database based authentication

--- src/main/java/edu/mum/employeerestcrud/config/SecurityConfig.java
+++ src/main/java/edu/mum/employeerestcrud/config/SecurityConfig.java
@@ -1,5 +1,7 @@
 package edu.mum.employeerestcrud.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -7,20 +9,41 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.User.UserBuilder;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+import javax.sql.DataSource;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
+    @Autowired
+    private BCryptPasswordEncoder bCryptPasswordEncoder;
+
+    @Autowired
+    private DataSource dataSource;
+
+    @Value("${spring.queries.users-query}")
+    private String usersQuery;
+
+    @Value("${spring.queries.roles-query}")
+    private String rolesQuery;
+
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+//        // in-memory authentication
+//        UserBuilder users = User.withDefaultPasswordEncoder();
+//        auth.inMemoryAuthentication()
+//                .withUser(users.username("dave").password("test123").roles("EMPLOYEE"))
+//                .withUser(users.username("betty").password("test123").roles("EMPLOYEE", "MANAGER"))
+//                .withUser(users.username("admin").password("test123").roles("EMPLOYEE", "ADMIN"));
 
-        UserBuilder users = User.withDefaultPasswordEncoder();
-
-        auth.inMemoryAuthentication()
-                .withUser(users.username("dave").password("test123").roles("EMPLOYEE"))
-                .withUser(users.username("betty").password("test123").roles("EMPLOYEE", "MANAGER"))
-                .withUser(users.username("admin").password("test123").roles("EMPLOYEE", "ADMIN"));
+        auth.
+                jdbcAuthentication()
+                .usersByUsernameQuery(usersQuery)
+                .authoritiesByUsernameQuery(rolesQuery)
+                .dataSource(dataSource)
+                .passwordEncoder(bCryptPasswordEncoder);
     }
 
     @Override
--- src/main/java/edu/mum/employeerestcrud/repository/EmployeeRepository.java
+++ src/main/java/edu/mum/employeerestcrud/repository/EmployeeRepository.java
@@ -2,7 +2,10 @@ package edu.mum.employeerestcrud.repository;
 
 import edu.mum.employeerestcrud.entity.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import org.springframework.web.bind.annotation.RequestBody;
 
+@Repository
 public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
 	
 }
